2b39485efcbb53eacef608e34d8ec3984df26b25
Mystic <58482090+PokeyBoa@users.noreply.github.com>
2022-06-26 20:24:20 +0800

    Update ip-helper.sh

--- shell/cmdline_params_parsing/ip-helper.sh
+++ shell/cmdline_params_parsing/ip-helper.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# Verify whether ip is a legal parameter
+check_ip() {
+    ipaddr=${1}
+    valid_check=$(echo $ipaddr | awk -F '.' '$1<=255 && $2<=255 && $3<=255 && $4<=255{print "yes"}')
+    if echo $ipaddr | grep -E "^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$" > /dev/null; then
+        if [[ ${valid_check:-no} == "yes" ]]; then
+            continue
+        else
+            echo -e "\nPlease enter a legal ipv4 address!\n"
+            exit 1
+        fi
+    else
+        echo -e "\nPlease enter a correct ipv4 format!\n"
+        exit 1
+    fi
+}
+
+
 # Command line parameter processing
 help_info="
 Usage: sh ${0} [OPTION]...
@@ -14,8 +32,8 @@ For example:
 "
 err_info="Try '${0} -h' for more information."
 
-if [[ ${#} -eq 0 ]]; then
-    echo -e "${err_info}\n"
+if [[ ${#} -eq 0 ]] || [[ ${#} -gt 2 ]]; then
+    echo -e "\n${err_info}\n"
     exit 99
 fi
 
@@ -24,13 +42,14 @@ do
     case $args in
         i)
             ip=${OPTARG}
+            check_ip ${ip}
         ;;
         h)
             echo "${help_info}"
             exit 0
         ;;
         *)
-            echo -e "${err_info}\n"
+            echo -e "\n${err_info}\n"
             exit 99
     esac
 done
