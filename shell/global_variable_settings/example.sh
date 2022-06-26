16cdb9e39381725a442083f4cf40cd202643297f
Mystic <58482090+PokeyBoa@users.noreply.github.com>
2022-06-26 19:44:21 +0800

    Update example.sh

--- shell/global_variable_settings/example.sh
+++ shell/global_variable_settings/example.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 #written on xx/xx
 
-set -o nounset                                  # set -u 引用了未定义的变量会报错
-export LANG=en_US.UTF-8                         # 设定语序环境
-stty erase '^h'                                 # 退格键操作
-trap 'echo -e "  \nExit...\n"' EXIT             # SIGINT信号捕获
-datesuf=`date +%Y%m%d`                          # 日期后缀
-symbols=`perl -e "print '#' x 50;"`             # 分割线
+set -o nounset
+export LANG=en_US.UTF-8
+stty erase '^h'
+trap 'echo -e "  \nExit...\n"' EXIT
+datesuf=`date +%Y%m%d`
+symbols=`perl -e "print '#' x 50;"`
 curdir=`dirname $(readlink -f $0)`
 basedir=`dirname $curdir`"/"
 
